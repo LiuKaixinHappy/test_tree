df9d334bcb242192bf25462a24f631ff98ee8f79
snowball <13681115889>
2019-11-01 17:24:59 +0800

    add find father brother

--- main/main.js
+++ main/main.js
@@ -26,7 +26,7 @@ const get_new_node_set = (question_tree_structure, step_teach_info) => {
                 id: child.id,
                 type: child.type,
                 is_only_child: curNode.children.length <= 1,
-                next_brother: curNode.children[index + 1] ? curNode.children[index + 1] : null,
+                next_brother: curNode.children[index + 1] ? curNode.children[index + 1] : curNode.father ? curNode.father.next_brother.children.every(item => item.type === 1) ? curNode.father.next_brother : null : null,
             };
             new_node_set.push(new_child_node);
             q.push(child);
@@ -69,148 +69,195 @@ const divide_children_by_important_step = (important_children_ids, children, max
 };
 
 const create_a_node_with_children = (new_children, new_order, max_id) => {
-  return {
-    id: max_id.toString(),
-    type: 0,
-    order: new_order,
-    children: new_children,
-  };
+    return {
+        id: max_id.toString(),
+        type: 0,
+        order: new_order,
+        children: new_children,
+    };
 };
 
 const find_father_in_tree = (node_id, tree) => {
-  const q = [];
-  q.push(tree);
-  while (q.length !== 0) {
-    const curNode = q.shift();
-    if (curNode.children.filter(item => item.id === node_id).length > 0) {
-      return curNode;
+    const q = [];
+    q.push(tree);
+    while (q.length !== 0) {
+        const curNode = q.shift();
+        if (curNode.children.filter(item => item.id === node_id).length > 0) {
+            return curNode;
+        }
+        curNode.children.forEach(child => q.push(child));
     }
-    curNode.children.forEach(child => q.push(child));
-  }
-  return undefined;
+    return undefined;
 };
 
-const resetOrder = (tree) => {
-  const fn = (child) => {
-    if (!child.children) {
-      return;
+const find_father_next_brother_in_tree = (node_id, new_node_set, tree) => {
+    console.log('94------------------',node_id);
+    let father_next_brother_id = undefined
+    for(let item of new_node_set){
+        if (item.children_ids.includes(node_id)) {
+            father_next_brother_id = item.next_brother;
+            break;
+        }
     }
-    child.children.map((item, i) => {
-      item.order = i + 1;
-      if (item.children.length > 0) {
-        fn(item);
-      }
-    });
-  };
-  fn(tree);
-  return tree;
+    // new_node_set.forEach(item => {
+        
+    // });
+    console.log('101--------------------',father_next_brother_id);
+    const q = [];
+    q.push(tree);
+    while (q.length !== 0) {
+        const curNode = q.shift();
+        if (curNode.id === father_next_brother_id) {
+            // console.log(curNode);
+            return curNode;
+        }
+        curNode.children.forEach(child => q.push(child));
+    }
+    return undefined;
+}
+
+const resetOrder = (tree) => {
+    const fn = (child) => {
+        if (!child.children) {
+            return;
+        }
+        child.children.map((item, i) => {
+            item.order = i + 1;
+            if (item.children.length > 0) {
+                fn(item);
+            }
+        });
+    };
+    fn(tree);
+    return tree;
 };
 
 const main = (question_tree_structure, step_teach_info, max_structure_id) => {
-  const new_node_set = get_new_node_set(question_tree_structure, step_teach_info);
-  const created_thought_ids = [];
-  const step_list = ['1','2','3','4','5'];
-  console.log('--------------------------------',step_teach_info)
-  if(step_list.every(item => step_teach_info[item].is_important===false)){
-      // TODO 是否所有步骤都不是重要步骤
-      const temp_step_list = [];
-      question_tree_structure.children.forEach((item) => {
-        if(item.type === 0){
-            item.children.forEach(step => {
-                temp_step_list.push(step);
-            })
-        } else {
-            temp_step_list.push(item);
-        }
-      })
-      question_tree_structure.children = temp_step_list;
-      return resetOrder(question_tree_structure);
-  }
-  for (const each of new_node_set) {
-    if (each.id === '0') {
-      // 1-都是步骤，0-都是思路，2-混合
-      if (each.children_type === 1) {
-        question_tree_structure.children = divide_children_by_important_step( each.important_children_ids, each.children, max_structure_id);
-        max_structure_id += 1;
-      } else if (each.children_type === 2) {
-        let tmp_arr = [];
-        const new_children = [];
-        let order = 1;
-        let sub_order = 1;
-        for (const child of each.children) {
-          // 如果是思路
-          if (child.type === 0) {
-            child.order = order;
-            order += 1;
-            new_children.push(child);
-            continue;
-          }
-          // 步骤，加入临时数组
-          child.order = sub_order;
-          tmp_arr.push(child);
-          sub_order += 1;
-          if (step_teach_info[child.id].is_important) {
-            new_children.push(create_a_node_with_children(tmp_arr, order, max_structure_id));
-            created_thought_ids.push(max_structure_id);
-            order += 1;
-            tmp_arr = [];
-          }
-        }
-        tmp_arr.forEach((item) => {
-          item.order = order;
-          new_children.push(item);
-          order += 1;
-        });
-        question_tree_structure.children = new_children;
-      }
-    } else if (each.children_type === 1) {
-      const father = find_father_in_tree(each.id, question_tree_structure);
-      const new_children = [];
-      // 如果有重要步骤
-      if (each.important_children_ids.length > 0) {
-        // 如果最后一步是隐藏步骤，其他都不是，跳出
-        if (each.important_children_ids.length === 1 && each.important_children_ids[0] === each.children_ids[each.children_ids.length - 1]) {
-          continue;
-        }
-        const groups = divide_children_by_important_step(each.important_children_ids, each.children, max_structure_id);
-        console.log('groups', groups);
-        created_thought_ids.push(max_structure_id);
-        for (const child of father.children) {
-          if (child.id === each.id) {
-            groups.forEach(item => new_children.push(item));
-          } else {
-            new_children.push(child);
-          }
-        }
-        father.children = new_children;
-      } else {
-        if (each.next_brother) {
-          for (const child of father.children) {
-            if (child.id === each.id) {
-              continue;
-            }
-            if (child.id === each.next_brother.id) {
-              each.children.forEach((item,index) => each.next_brother.children.splice(index, 0, item));
-              new_children.push(each.next_brother);
+    const new_node_set = get_new_node_set(question_tree_structure, step_teach_info);
+    const created_thought_ids = [];
+    const step_list = ['1', '2', '3', '4', '5'];
+    if (step_list.every(item => !step_teach_info[item].is_important)) {
+        // TODO 是否所有步骤都不是重要步骤
+        const temp_step_list = [];
+        question_tree_structure.children.forEach((item) => {
+            if (item.type === 0) {
+                item.children.forEach(step => {
+                    temp_step_list.push(step);
+                })
             } else {
-              new_children.push(child);
+                temp_step_list.push(item);
             }
-          }
-          father.children = new_children;
-        } else {
-          const arr = [];
-          for (const c of father.children) {
-            if (c.id !== each.id) {
-              arr.push(c);
-            }
-          }
-          father.children = arr;
-          each.children.forEach(item => question_tree_structure.children.push(item));
-        }
-      }
+        })
+        question_tree_structure.children = temp_step_list;
+        return resetOrder(question_tree_structure);
     }
-  }
-  return resetOrder(question_tree_structure);
+    // console.log('----------------------', JSON.stringify(new_node_set));
+    for (const each of new_node_set) {
+
+        if (each.id === '0') {
+            // 1-都是步骤，0-都是思路，2-混合
+            if (each.children_type === 1) {
+                question_tree_structure.children = divide_children_by_important_step(each.important_children_ids, each.children, max_structure_id);
+                max_structure_id += 1;
+            } else if (each.children_type === 2) {
+                let tmp_arr = [];
+                const new_children = [];
+                let order = 1;
+                let sub_order = 1;
+                for (const child of each.children) {
+                    // 如果是思路
+                    if (child.type === 0) {
+                        child.order = order;
+                        order += 1;
+                        new_children.push(child);
+                        continue;
+                    }
+                    // 步骤，加入临时数组
+                    child.order = sub_order;
+                    tmp_arr.push(child);
+                    sub_order += 1;
+                    if (step_teach_info[child.id].is_important) {
+                        new_children.push(create_a_node_with_children(tmp_arr, order, max_structure_id));
+                        created_thought_ids.push(max_structure_id);
+                        order += 1;
+                        tmp_arr = [];
+                    }
+                }
+                tmp_arr.forEach((item) => {
+                    item.order = order;
+                    new_children.push(item);
+                    order += 1;
+                });
+                question_tree_structure.children = new_children;
+            }
+        } else if (each.children_type === 1) {
+            const father = find_father_in_tree(each.id, question_tree_structure);
+            const new_children = [];
+            // 如果有重要步骤
+            if (each.important_children_ids.length > 0) {
+                // 如果最后一步是隐藏步骤，其他都不是，跳出
+                if (each.important_children_ids.length === 1 && each.important_children_ids[0] === each.children_ids[each.children_ids.length - 1]) {
+                    continue;
+                }
+                const groups = divide_children_by_important_step(each.important_children_ids, each.children, max_structure_id);
+                // console.log('groups', groups);
+                created_thought_ids.push(max_structure_id);
+                for (const child of father.children) {
+                    if (child.id === each.id) {
+                        groups.forEach(item => new_children.push(item));
+                    } else {
+                        new_children.push(child);
+                    }
+                }
+                father.children = new_children;
+            } else {
+                // console.log('----------------------------', JSON.stringify(father));
+                const father_brother = find_father_next_brother_in_tree(each.id,new_node_set,question_tree_structure);
+                if (each.next_brother) {
+                    for (const child of father.children) {
+                        if (child.id === each.id) {
+                            continue;
+                        }
+                        if (child.id === each.next_brother.id) {
+                            each.children.forEach((item, index) => each.next_brother.children.splice(index, 0, item));
+                            new_children.push(each.next_brother);
+                        } else {
+                            new_children.push(child);
+                        }
+                    }
+                    father.children = new_children;
+                } else if (father_brother.children.every(item => item.type === 1)) {
+                    for (const child of father.children) {
+                        if (child.id !== each.id) {
+                            // continue;
+                            new_children.push(child);
+                        }
+                    }
+                    father.children = new_children;
+                    each.children.forEach((item, index) => father_brother.children.splice(index, 0, item));
+                } else if (each.father.next_brother && each.father.next_brother.children[0].type === 0 && each.father.next_brother.children[0].children.every(item => item.type === 1)) {
+                    each.father.forEach(item => {
+                        if (item.id !== each.id) {
+                            // continue;
+                            new_children.push(item);
+                        }
+                    })
+                    father.children = new_children;
+                    each.children.forEach((item, index) => each.father.next_brother.children.splice(index, 0, item));
+                } else {
+                    const arr = [];
+                    for (const c of father.children) {
+                        if (c.id !== each.id) {
+                            arr.push(c);
+                        }
+                    }
+                    father.children = arr;
+                    each.children.forEach(item => question_tree_structure.children.push(item));
+                }
+            }
+        }
+    }
+    return resetOrder(question_tree_structure);
 };
 
-module.exports = {main, divide_children_by_important_step};
+module.exports = { main, divide_children_by_important_step };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1462,4 +1462,183 @@ describe('main()', () => {
     expect(JSON.stringify(main.main(question_tree_structure, step_teach_info, max_id))).toBe(expected);
   });
 
+  it('思路中最后一个子思路中的步骤被删除，且父亲节点的兄弟节点为底层思路，移动到父亲节点的兄弟节点', () => {
+    const question_tree_structure = {
+      "id": "0",
+      "type": 0,
+      "order": 1,
+      "children": [
+        {
+          "id": "10",
+          "type": 0,
+          "order": 1,
+          "children": [
+            {
+              "id": "8",
+              "type": 0,
+              "order": 1,
+              "children": [{
+                "id": "1",
+                "type": 1,
+                "order": 1,
+                "children": []
+              },{
+                "id": "2",
+                "type": 1,
+                "order": 2,
+                "children": []
+              },{
+                "id": "3",
+                "type": 1,
+                "order": 3,
+                "children": []
+              }]
+            },{
+              "id": "9",
+              "type": 0,
+              "order": 2,
+              "children": [{
+                "id": "4",
+                "type": 1,
+                "order": 1,
+                "children": []
+              }]
+            }
+            
+          ]
+        },{
+          "id": "7",
+          "type": 0,
+          "order": 2,
+          "children": [
+            {
+              "id": "5",
+              "type": 1,
+              "order": 1,
+              "children": []
+            },{
+              "id": "6",
+              "type": 1,
+              "order": 2,
+              "children": []
+            }
+          ]
+        },
+        
+      ]
+    };
+    const step_teach_info = {
+      "1": {
+        "text": "由题知: $AD\\parallel EC$, 且$EC=\\frac12BC=2=AD$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "2": {
+        "text": "$\\therefore$四边形$ADEC$是平行四边形\n$\\therefore AE=DC=AB=BE=2$\n$\\therefore \\triangle ABE$是等边三角形\n$\\therefore MB_1\\perp AE$, $DM\\perp AE$ \n以$ME$为$x$轴, $MD$为$y$轴, $MB_{1}$为$z$轴建立空间直角坐标系\n$C(2, 2, 0)$, $B_{1}(0, 0, \\sqrt3)$, $A(-1, 0, 0)$, $D(0, \\sqrt3, 0) $\n$\\vec{DA}=(-1,-\\sqrt3,0)$, $\\vec{BA}=(-1,0,-\\sqrt3)$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "3": {
+        "text": "平面$AB_{1}E$的法向量为$\\vec{n}=(0, 1, 0)$\n设平面$DB_{1}A$的法向量为$\\vec m=(x, y, z)$\n得: $\\begin{cases} \\vec{m} \\cdot \\vec{DA} =0 \\\\ \\vec{m} \\cdot \\vec{B_1A} = 0 \\end{cases}$",
+        "topic": null,
+        "is_important": true,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "4": {
+        "text": "解得: $\\begin{cases} y=z\\\\x=-\\sqrt3y\\end{cases}$, 取$y=1$, 得: $\\vec{m}=(-\\sqrt3, 1, 1)$\n$\\therefore\\cos\\lt\\vec{n},\\vec{m}\\gt =\\frac{\\vec{n}\\cdot\\vec{m}}{|\\vec{n}|\\cdot|\\vec{m}|}= \\frac{\\sqrt{5}}5$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "5": {
+        "text": "$\\because $二面角$D-AB_{1}-E$为锐角\n$\\therefore$余弦值为$ \\frac{\\sqrt{5}}5$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "6": {
+        "text": "$\\because $二面角$D-AB_{1}-E$为锐角\n$\\therefore$余弦值为$ \\frac{\\sqrt{5}}5$",
+        "topic": null,
+        "is_important": true,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      }
+    };
+    const max_id = 11;
+    const expected = JSON.stringify({
+      "id": "0",
+      "type": 0,
+      "order": 1,
+      "children": [
+        {
+          "id": "10",
+          "type": 0,
+          "order": 1,
+          "children": [
+            {
+              "id": "8",
+              "type": 0,
+              "order": 1,
+              "children": [{
+                "id": "1",
+                "type": 1,
+                "order": 1,
+                "children": []
+              },{
+                "id": "2",
+                "type": 1,
+                "order": 2,
+                "children": []
+              },{
+                "id": "3",
+                "type": 1,
+                "order": 3,
+                "children": []
+              }]
+            }
+          ]
+        },{
+          "id": "7",
+          "type": 0,
+          "order": 2,
+          "children": [
+            {
+              "id": "4",
+              "type": 1,
+              "order": 1,
+              "children": []
+            },
+            {
+              "id": "5",
+              "type": 1,
+              "order": 2,
+              "children": []
+            },{
+              "id": "6",
+              "type": 1,
+              "order": 3,
+              "children": []
+            }
+          ]
+        },
+        
+      ]
+    });
+
+    expect(JSON.stringify(main.main(question_tree_structure, step_teach_info, max_id))).toBe(expected);
+  });
+
 });
