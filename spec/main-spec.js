6bd9678e7511dfb4ace0e317ef3dedfaa1628ed8
LiuKaixin <644306737@qq.com>
2019-11-01 18:06:26 +0800

    add test case

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1858,4 +1858,131 @@ describe('main()', () => {
 
     expect(JSON.stringify(main.main(question_tree_structure, step_teach_info, max_id))).toBe(expected);
   });
+
+  it('删除子思路的唯一重要步骤，其父亲没有兄弟节点，则插到该思路下', () => {
+    const question_tree_structure = {
+      "id": "0",
+      "type": 0,
+      "order": 1,
+      "children": [
+        {
+          "id": "10",
+          "type": 0,
+          "order": 1,
+          "children": [
+            {
+              "id": "8",
+              "type": 0,
+              "order": 1,
+              "children": [{
+                "id": "1",
+                "type": 1,
+                "order": 1,
+                "children": []
+              },{
+                "id": "2",
+                "type": 1,
+                "order": 2,
+                "children": []
+              },{
+                "id": "3",
+                "type": 1,
+                "order": 3,
+                "children": []
+              }]
+            },
+            {
+              "id": "9",
+              "type": 0,
+              "order": 2,
+              "children": [{
+                "id": "4",
+                "type": 1,
+                "order": 1,
+                "children": []
+              }]
+            }
+          ]
+        }
+      ]
+    };
+    const step_teach_info = {
+      "1": {
+        "text": "由题知: $AD\\parallel EC$, 且$EC=\\frac12BC=2=AD$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "2": {
+        "text": "$\\therefore$四边形$ADEC$是平行四边形\n$\\therefore AE=DC=AB=BE=2$\n$\\therefore \\triangle ABE$是等边三角形\n$\\therefore MB_1\\perp AE$, $DM\\perp AE$ \n以$ME$为$x$轴, $MD$为$y$轴, $MB_{1}$为$z$轴建立空间直角坐标系\n$C(2, 2, 0)$, $B_{1}(0, 0, \\sqrt3)$, $A(-1, 0, 0)$, $D(0, \\sqrt3, 0) $\n$\\vec{DA}=(-1,-\\sqrt3,0)$, $\\vec{BA}=(-1,0,-\\sqrt3)$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "3": {
+        "text": "平面$AB_{1}E$的法向量为$\\vec{n}=(0, 1, 0)$\n设平面$DB_{1}A$的法向量为$\\vec m=(x, y, z)$\n得: $\\begin{cases} \\vec{m} \\cdot \\vec{DA} =0 \\\\ \\vec{m} \\cdot \\vec{B_1A} = 0 \\end{cases}$",
+        "topic": null,
+        "is_important": true,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      },
+      "4": {
+        "text": "解得: $\\begin{cases} y=z\\\\x=-\\sqrt3y\\end{cases}$, 取$y=1$, 得: $\\vec{m}=(-\\sqrt3, 1, 1)$\n$\\therefore\\cos\\lt\\vec{n},\\vec{m}\\gt =\\frac{\\vec{n}\\cdot\\vec{m}}{|\\vec{n}|\\cdot|\\vec{m}|}= \\frac{\\sqrt{5}}5$",
+        "topic": null,
+        "is_important": false,
+        "is_hidden": false,
+        "image_list": [],
+        "important_text_locations": []
+      }
+    };
+    const max_id = 14;
+    const expected = JSON.stringify({
+      "id": "0",
+      "type": 0,
+      "order": 1,
+      "children": [
+        {
+          "id": "10",
+          "type": 0,
+          "order": 1,
+          "children": [
+            {
+              "id": "8",
+              "type": 0,
+              "order": 1,
+              "children": [{
+                "id": "1",
+                "type": 1,
+                "order": 1,
+                "children": []
+              }, {
+                "id": "2",
+                "type": 1,
+                "order": 2,
+                "children": []
+              }, {
+                "id": "3",
+                "type": 1,
+                "order": 3,
+                "children": []
+              }]
+            }
+          ]
+        },
+        {
+          "id": "4",
+          "type": 1,
+          "order": 2,
+          "children": []
+        }
+      ]
+    });
+
+    expect(JSON.stringify(main.main(question_tree_structure, step_teach_info, max_id))).toBe(expected);
+  });
 });
